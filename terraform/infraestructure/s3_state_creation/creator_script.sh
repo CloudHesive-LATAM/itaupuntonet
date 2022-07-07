f551872d00a238fdb770121aa617d54f11920aac
fidelgonzalez <fidel.gonzalez@cloudhesive.com>
2022-07-07 20:56:39 -0300

    alpine papa

--- terraform/infraestructure/s3_state_creation/creator_script.sh
+++ terraform/infraestructure/s3_state_creation/creator_script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+bucket_name=$1
+dynamodb_table_name=$2
 echo " -------------------- "
 echo " Definited PARAMETERS "
 echo "BucketName: $bucket_name"
@@ -12,5 +14,5 @@ else
     cd terraform/infraestructure/s3_state_creation; 
     terraform init; 
     terraform plan -var "bucket_state_name=$bucket_name" -var "dynamodb_table_name=$dynamodb_table_name" -out=plan.out; 
-    terraform apply plan.out
+    #terraform apply plan.out
 fi
