0eb7f4ff60765d98f29fb29b16870595d1f4d841
Matias <matiaspalmero@live.com>
2022-07-07 18:25:08 -0300

    fdsfdsfds

--- terraform/infraestructure/application/provider.tf
+++ terraform/infraestructure/application/provider.tf
@@ -17,3 +17,27 @@ provider "aws" {
   region     = var.aws_region
 
 }
+
+provider "aws" {
+  region     = var.aws_region
+  alias = "cuenta-shared"
+
+}
+provider "aws" {
+  # The security credentials for AWS Account A.
+  region  = var.aws_region["virginia"]
+  # profile = var.aws_provider_profile
+  alias = "sts-shared-security"
+    assume_role {
+    role_arn = var.role_arn_sts_shared
+  }
+  # (Optional) the MFA token for Account A.
+  # token      = "123456"
+  # assume_role {
+  #   # The role ARN within Account B to AssumeRole into. Created in step 1.
+  #   role_arn    = var.sts["role"]
+  #   # (Optional) The external ID created in step 1c.
+  #   #external_id = "my_external_id"
+  #   session_name = var.sts["session_name"]
+  # }
+}
