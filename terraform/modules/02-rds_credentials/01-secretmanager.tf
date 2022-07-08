58c761c28677647f00dfeb27d39805a7e750e398
fidelgonzalez <fidel.gonzalez@cloudhesive.com>
2022-07-08 16:24:55 -0300

    alpine padfaadsfpa

--- terraform/modules/02-rds_credentials/01-secretmanager.tf
+++ terraform/modules/02-rds_credentials/01-secretmanager.tf
@@ -16,11 +16,11 @@ resource "aws_secretsmanager_secret_version" "password" {
   secret_string = random_password.master.result
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  provider = aws
-  bucket = "fafaidfaseiufdxszoivjasidofuasd12321dfdf3"
+# resource "aws_s3_bucket" "terraform_state" {
+#   provider = aws # dev
+#   bucket = "fafaidfaseiufdxszoivjasidofuasd12321dfdf3"
 
-  lifecycle {
-    prevent_destroy = true
-  }
-}
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
