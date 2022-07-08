1cf3dda1d960ed251b76506cb79c02357731f770
fidelgonzalez <fidel.gonzalez@cloudhesive.com>
2022-07-08 12:25:25 -0300

    alpine papa

--- terraform/infraestructure/application/02-root.tf
+++ terraform/infraestructure/application/02-root.tf
@@ -40,4 +40,13 @@ data "aws_caller_identity" "parent" {
 
 data "aws_caller_identity" "child" {
   provider = aws.sts_shared_account
+
+}
+
+output "account_id_child" {
+  value = data.aws_caller_identity.child.account_id
+}
+
+output "account_id_parent" {
+  value = data.aws_caller_identity.parent.account_id
 }
