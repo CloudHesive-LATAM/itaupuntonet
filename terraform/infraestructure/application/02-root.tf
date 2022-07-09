8447d08ff55038ad361ad589720290ef40468084
Matias <matiaspalmero@live.com>
2022-07-09 19:35:34 -0300

    se agrega base de datos

--- terraform/infraestructure/application/02-root.tf
+++ terraform/infraestructure/application/02-root.tf
@@ -1,15 +1,15 @@
-# module "rds" {
-#   source            = "../../modules/rds"
+module "rds" {
+  source            = "../../modules/rds"
   
-#   allocated_storage    = "100"
-#   engine               = "14.1"
-#   engine_version       = "postgres"
-#   instance_class       = "m5.large"
-#   name                 = "itaupuntonet"
-#   username             = "dbadmin"
-#   password             = data.aws_secretsmanager_secret_version.password
-#   parameter_group_name = "default"
-#   skip_final_snapshot  = true
+  allocated_storage    = var.allocated_storage 
+  engine               = var.engine
+  engine_version       = var.engine_version
+  instance_class       = var.instance_class
+  name                 = var.name
+  username             = var.username
+  password             = data.aws_secretsmanager_secret_version.password
+  parameter_group_name = "default"
+  skip_final_snapshot  = true
 
 
 module "rds_credentials" {
