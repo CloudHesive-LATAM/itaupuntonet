137def2472ce2508b8d72fec6e0ea0ef5fa8a8d4
Matias <matiaspalmero@live.com>
2022-07-09 12:50:19 -0300

    fdsfdsfds

--- terraform/infraestructure/application/02-root.tf
+++ terraform/infraestructure/application/02-root.tf
@@ -11,41 +11,6 @@
 #   parameter_group_name = "default"
 #   skip_final_snapshot  = true
 
-# }
-
-
-# provider "aws" {
-#   region = "us-east-2"
-#   alias  = "parent"
-# }
-
-# provider "aws" {
-#   region = "us-east-2"
-#   alias  = "child"
-
-#   assume_role {
-#     role_arn = var.child_iam_role_arn
-#   }
-# }
-
-# TOMAR DATA PARA VER Q SE PUEDA ASUMIR EL ROL
-# data "aws_caller_identity" "parent" {
-#   provider = aws
-# }
-
-# data "aws_caller_identity" "child" {
-#   provider = aws.sts_shared_account
-
-# }
-
-# output "account_id_child" {
-#   value = data.aws_caller_identity.child.account_id
-# }
-
-# output "account_id_parent" {
-#   value = data.aws_caller_identity.parent.account_id
-# }
-
 
 module "rds_credentials" {
    source            = "../../modules/02-rds_credentials"
